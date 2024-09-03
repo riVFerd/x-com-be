9515aa09d8b7bd431e41109e73b52a8173d6cdec
Virgy Ferdian <virgyferdian213@gmail.com>
2024-09-03 22:52:27 +0700

    feat: basic functionality

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,36 @@ import {WSContext} from "hono/dist/types/helper/websocket";
 
 const app = new Hono()
 const {upgradeWebSocket, websocket} = createBunWebSocket()
-const users = new Set<WSContext>();
 
-app.get('/', (c) => {
-    return c.json({message: 'Hello World'})
-})
+const chatRooms = {} as Record<string, Set<WSContext>>;
+
+app.get(
+    '/x-com/:roomId',
+    upgradeWebSocket((c) => {
+        const roomId = c.req.param('roomId');
+        return {
+            onOpen(event, ws) {
+                if (!chatRooms[roomId]) {
+                    console.log('Connection opened for room:', roomId);
+                    chatRooms[roomId] = new Set([ws]);
+                } else {
+                    chatRooms[roomId].add(ws);
+                }
+            },
+            onMessage(event, ws) {
+                const data = JSON.parse(event.data as string);
+                const message = `${data.username}: ${data.message}`;
+                chatRooms[roomId].forEach((user) => {
+                    user.send(JSON.stringify(message));
+                });
+            },
+            onClose: (event, ws) => {
+                console.log('Connection closed')
+                chatRooms[roomId].delete(ws);
+            },
+        }
+    })
+)
 
 Bun.serve({
     fetch: app.fetch,
